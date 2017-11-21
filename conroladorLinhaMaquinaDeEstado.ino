983b605d20c0b145aeb1715f9234ef01cbb8b113
Gabriel Marques <gabriel1997.marques97@gmail.com>
2017-11-21 20:08:00 -0300

    Update conroladorLinhaMaquinaDeEstado.ino

--- conroladorLinhaMaquinaDeEstado.ino
+++ conroladorLinhaMaquinaDeEstado.ino
@@ -10,10 +10,11 @@
 #define Motor_D1 3
 #define SENOP 2
 
+#define TEMPO 1000
+
 Ultrasonic ultrasonic(9,10);
 
 void setup(){
-  Serial.begin(9600);
   pinMode(echoPin, INPUT); // define o pino 10 como entrada (recebe)
   pinMode(trigPin, OUTPUT); // define o pino 9 como saida (envia)
    initPins();
@@ -75,14 +76,6 @@ int calDist() {
   return distancia;
 }
 
-void swap(int *vel1, int *vel2){
-  int aux=*vel1;
-  
-  *vel1=*vel2;
-  
-  *vel2=aux;
-}
-
 void seguirLinha(){
   static int estado=0;
   static boolean outInercia1=true, outInercia2=true, tryCorrec1=false, tryCorrec2=false;
@@ -91,16 +84,17 @@ void seguirLinha(){
     case 0: if(valSensor()==0){
               estado=1;
               
-              if(!tryCorrec1){
+              //if(!tryCorrec1){
                 moveMotors(80, 0);
-                delay(200);
-              }
-              else{
+                delay(50);
+              //}
+              
+              /*else{
                 stopMotors();
                 delay(250);
                 moveMotors(80, 0);
                 delay(100);
-              }
+              }*/
               
               stopMotors();
 
@@ -127,14 +121,15 @@ void seguirLinha(){
               
               if(!tryCorrec2){
                 moveMotors(0, 80);
-                delay(200); 
+                delay(50); 
               }
-              else{
+              
+              /*else{
                 stopMotors();
                 delay(250);
                 moveMotors(0, 80);
                 delay(100);
-              }
+              }*/
               
               stopMotors();
 
@@ -158,50 +153,66 @@ void seguirLinha(){
   }
 }
 
-void loop() {
-  int distancia=calDist();
+void boost(char side){
+  switch(side){
+    case 'd': moveMotors(100, -100);
+            break;
+
+    case 'e': moveMotors(-100, 100);
+            break;        
+
+    case 'f': moveMotors(100, 100);
+            break;        
+  }
+
+  delay(50);
+}
+
+void mover(char side, int tempo){
+  boost(side);
   
-  if(distancia<=8){
-    stopMotors();
-    moveMotors(-100,100);
-    delay(50);
-    moveMotors(-50,50);
-    while(calDist()<20); 
-    delay(200);
-    stopMotors();
-    
-    moveMotors(100, 100);//move
-    delay(50);
-    moveMotors(60,50);
-    delay(1000);
+  switch(side){
+    case 'd': moveMotors(50, -50);
+              break;
+
+    case 'e': moveMotors(-50, 50);
+              break;   
+
+    case 'f': moveMotors(50, 60);
+              break;             
+  }
+
+  delay(tempo);
+}
+
+void desviarObstaculo(){
     stopMotors();
 
-    moveMotors(100,-100);//angulo
-    delay(50);
-    moveMotors(50,-50);
-    delay(500);
+    mover('e', 0);
+    while(calDist()<20);
+    mover('e', TEMPO/5);
     stopMotors();
-    delay(3000);
-    
-    /*moveMotors(30,30);//move
-    delay(50);
-    moveMotors(30,30);
-    delay(500);
-    moveMotors(100,-100);//angulo
-    delay(50);
-    moveMotors(40,-40);
-    delay(500);
-    moveMotors(30,30);//move
-    delay(50);
-    moveMotors(30,30);
-    delay(500);
-    moveMotors(-100,100);//angulo
-    delay(50);
-    moveMotors(-40,40);
-    delay(500);
+
+    mover('f', TEMPO);
     stopMotors();
-    seguirLinha();*/
-  }else{
-    seguirLinha();
-  }
+
+    mover('d', TEMPO/2);
+    stopMotors();
+
+    mover('f', TEMPO);
+    stopMotors();
+
+    mover('e', TEMPO/5);
+    stopMotors();
+
+    mover('f', 0);
+    while(valSensor()==1);
+    stopMotors();
+}
+
+void loop() {
+  int distancia=calDist();
+
+  if(distancia<=8) desviarObstaculo();
+  else seguirLinha();
 }
