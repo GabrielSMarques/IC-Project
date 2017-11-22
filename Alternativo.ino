0bc3d2d10d5b41cfe4558e60afafc26112c47d9d
Gabriel Marques <gabriel1997.marques97@gmail.com>
2017-11-22 13:16:14 -0300

    Update Alternativo.ino

--- Alternativo.ino
+++ Alternativo.ino
@@ -1,7 +1,9 @@
-/*#include "Ultrasonic.h"
+#include "Ultrasonic.h"
 #define echoPin 10 //Pino 10 recebe o pulso do echo
 #define trigPin 9 //Pino 9 envia o pulso para gerar o echo
-*/
+
+Ultrasonic ultrasonic(9,10);
+
 #define Motor_E1 8
 #define Motor_E2 7
 #define Motor_EV 6
@@ -13,6 +15,8 @@
 #define FORCAM 55
 #define FORCAm 0
 #define FORCAmed 15
+
+#define TEMPO 500
 
 void setup(){
    initPins();
@@ -26,23 +30,15 @@ void initPins(){
   pinMode(Motor_D1, OUTPUT);
   pinMode(Motor_D2, OUTPUT);
 
-  /*pinMode(echoPin, INPUT);
-  pinMode(trigPin. OUTPUT);
-*/
+  pinMode(echoPin, INPUT);
+  pinMode(trigPin, OUTPUT);
+
   pinMode(SENOP, INPUT);
 }
 
-void loop() {
+void seguidorDeLinha(){
   static int estado = 0;
   static double forcaD, forcaE, linhaE, linhaD;
-  
-  /*
-  linhaD = 50, linhaE = 15;
-  if(valSensor()==0) {
-    moverMotor(linhaE,linhaD);  
-  } else {
-    moverMotor(linhaD,linhaE);
-  }*/
 
   if(valSensor()==0) {
     moverMotor(linhaE, linhaD);
@@ -71,10 +67,82 @@ void loop() {
   moverMotor(forcaE,forcaD);
 }
 
+void loop() {
+  int distancia=calDist();
+
+  if(distancia<=8) desviarObstaculo();
+  else seguidorDeLinha();
+}
+
+void boost(char side){
+  switch(side){
+    case 'd': moverMotor(100, -100);
+            break;
+
+    case 'e': moverMotor(-100, 100);
+            break;        
+
+    case 'f': moverMotor(100, 100);
+            break;        
+  }
+
+  delay(50);
+}
+
+void stopMotors(){
+  moverMotor(-50, -50);
+  delay(10);
+  moverMotor(50, 50);
+  delay(10);
+  moverMotor(0, 0);
+}
+
+void mover(char side, int tempo){
+  boost(side);
+  
+  switch(side){
+    case 'd': moverMotor(40, -40);
+              break;
+
+    case 'e': moverMotor(-40, 40);
+              break;   
+
+    case 'f': moverMotor(40, 40);
+              break;             
+  }
+
+  delay(tempo);
+}
+
+void desviarObstaculo(){
+    stopMotors();
+
+    mover('e', 0);
+    while(calDist()<20);
+    mover('e', TEMPO/5);
+    stopMotors();
+
+    mover('f', TEMPO);
+    stopMotors();
+
+    mover('d', TEMPO/2);
+    stopMotors();
+
+    mover('f', TEMPO);
+    stopMotors();
+
+    mover('e', TEMPO/5);
+    stopMotors();
+
+    mover('f', 0);
+    while(valSensor()==1);
+    stopMotors();
+}
+
 int valSensor(){
   return int(digitalRead(SENOP));
 }
-/*
+
 int calDist() {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
@@ -83,9 +151,8 @@ int calDist() {
   digitalWrite(trigPin, LOW);
   
   int distancia = (ultrasonic.Ranging(CM));
-
   return distancia;
-}*/
+}
 
 void moverMotor(double left, double right){
   if(left>0){
