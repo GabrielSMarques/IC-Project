3a3c0faaa33716414b98a96141c96a226c89f146
Gabriel Marques <gabriel1997.marques97@gmail.com>
2017-11-28 09:47:38 -0300

    Update Alternativo.ino

--- Alternativo.ino
+++ Alternativo.ino
@@ -1,8 +1,12 @@
 #include "Ultrasonic.h"
-#define echoPin 10 //Pino 10 recebe o pulso do echo
-#define trigPin 9 //Pino 9 envia o pulso para gerar o echo
 
 Ultrasonic ultrasonic(9,10);
+
+#define ledPinE 12
+#define ledPinD 11
+
+#define echoPin 10
+#define trigPin 9
 
 #define Motor_E1 8
 #define Motor_E2 7
@@ -12,15 +16,7 @@ Ultrasonic ultrasonic(9,10);
 #define Motor_D1 3
 #define SENOP 2
 
-#define FORCAM 55
-#define FORCAm 0
-#define FORCAmed 15
-
 #define TEMPO 500
-
-void setup(){
-   initPins();
-}
 
 void initPins(){
   pinMode(Motor_E1, OUTPUT);
@@ -36,105 +32,199 @@ void initPins(){
   pinMode(SENOP, INPUT);
 }
 
-void seguidorDeLinha(){
-  static int estado = 0;
-  static double forcaD, forcaE, linhaE, linhaD;
+void setup(){
+   initPins();
+}
 
-  if(valSensor()==0){    
-    while(valSensor()==0);
-    
-    switch (estado) {
-      case 0:
-        //agora ele está na esquerda
-        forcaE = FORCAM;
-        forcaD = FORCAm;
-        linhaE = FORCAM;
-        linhaD = FORCAmed;
-        //mudar o estado
-        estado = 1;
-        break;
-      case 1:
-        //agora ele está na direita
-        forcaE = FORCAm;
-        forcaD = FORCAM;
-        linhaD = FORCAM;
-        linhaE = FORCAmed;
-        //mudar o estado
-        estado = 0;
-        break;
+void swap(int *val1, int *val2){
+  int aux=*val1;
+
+  *val1=*val2;
+
+  *val2=aux;
+}
+
+void sort(int vetor[10]){
+   for(int i=0; i<10; i++){
+      for(int j=0; j<9; j++){
+         if(vetor[j]>vetor[j+1]) swap(&vetor[j], &vetor[j+1]);
+      }
+   }
+}
+
+int calDist(boolean filtro){
+  int distancia, vetor[10];
+  
+  if(filtro){
+    for(int c1=0; c1<10; c1++){
+      digitalWrite(trigPin, LOW);
+      delayMicroseconds(2);
+      digitalWrite(trigPin, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(trigPin, LOW);  
+  
+      vetor[c1] = (int)(ultrasonic.Ranging(CM));
     }
+
+    sort(vetor);
+
+    distancia=vetor[4];
+  }
+  else{
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);  
+
+    distancia = (int)(ultrasonic.Ranging(CM));
   }
   
-  moverMotor(forcaE,forcaD);
+  return distancia;
 }
 
-void loop() {
-  int distancia=calDist();
+void moverMotor(char side, double forca){
+  if(side=='e'){
+    
+    if(forca>0){
+      digitalWrite(Motor_E1, LOW);
+      digitalWrite(Motor_E2, HIGH);
+    }else{
+      digitalWrite(Motor_E1, HIGH);
+      digitalWrite(Motor_E2, LOW);
+    } 
 
-  if(distancia<=8) desviarObstaculo();
-  else seguidorDeLinha();
-}
-
-void boost(char side){
-  switch(side){
-    case 'd': moverMotor(100, -100);
-            break;
-
-    case 'e': moverMotor(-100, 100);
-            break;        
-
-    case 'f': moverMotor(100, 100);
-            break;        
+     analogWrite(Motor_EV, abs(forca));
+     
   }
+  else{
+    
+    if(forca>0){
+      digitalWrite(Motor_D1, HIGH);
+      digitalWrite(Motor_D2, LOW);
+    }else{
+      digitalWrite(Motor_D1, LOW);
+      digitalWrite(Motor_D2, HIGH);
+    }
 
-  delay(50);
+    analogWrite(Motor_DV, abs(forca));
+    
+  }
 }
 
 void stopMotors(){
-  moverMotor(-50, -50);
-  delay(10);
-  moverMotor(50, 50);
-  delay(10);
-  moverMotor(0, 0);
+  digitalWrite(ledPinE, HIGH);
+  digitalWrite(ledPinD, HIGH);
+
+  moverMotor('e', 0);
+  moverMotor('d', 0);
+}
+
+void boostMotor(char side, char direc){
+  if(direc=='f') moverMotor(side, 100);
+  else moverMotor(side, -100);
+
+  delay(25);
 }
 
 void mover(char side, int tempo){
-  boost(side);
-  
   switch(side){
-    case 'd': moverMotor(50, -50);
+    case 'd': moverMotor('d', -40);
+              moverMotor('e', 40);
               break;
 
-    case 'e': moverMotor(-50, 50);
+    case 'e': moverMotor('d', 40);
+              moverMotor('e', -40);
               break;   
 
-    case 'f': moverMotor(50, 50);
+    case 'f': moverMotor('d', 40);
+              moverMotor('e', 40);
               break;             
   }
 
-  delay(tempo);
+  unsigned int tempo1=millis(), tempo2=millis();
+  while(millis()-tempo<=tempo){
+    
+    if(millis()-tempo2<=500){
+      switch(side){
+        case 'e': digitalWrite(ledPinE, LOW);
+                  break;
+                  
+        case 'd': digitalWrite(ledPinD, LOW);
+                  break;
+
+        case 'f': digitalWrite(ledPinD, LOW);
+                  digitalWrite(ledPinE, LOW);
+                  break;
+      }
+    }
+    else if(millis()-tempo2<=1000){
+      switch(side){
+        case 'e': digitalWrite(ledPinE, HIGH);
+                  break;
+                  
+        case 'd': digitalWrite(ledPinE, HIGH);
+                  break;
+
+        case 'f': digitalWrite(ledPinD, LOW);
+                  digitalWrite(ledPinE, LOW);
+                  break;          
+      }
+    }
+    else tempo2=millis();
+    
+  }
 }
 
 void desviarObstaculo(){
     stopMotors();
 
+    boostMotor('e', 't');
+    boostMotor('d', 't');
+    while(calDist(true)<=10){
+      moverMotor('e', -40);
+      moverMotor('d', -40);
+    }
+
+    boostMotor('e', 't');
+    boostMotor('d', 'f');
     mover('e', 0);
-    while(calDist()<20);
+    while(calDist(true)<20);
     mover('e', TEMPO/5);
     stopMotors();
 
+    delay(2000);
+
+    boostMotor('e', 'f');
+    boostMotor('d', 'f');
     mover('f', TEMPO);
     stopMotors();
 
+    delay(2000);
+
+    boostMotor('e', 'f');
+    boostMotor('d', 't');
     mover('d', TEMPO/2);
     stopMotors();
 
+    delay(2000);
+
+    boostMotor('e', 'f');
+    boostMotor('d', 'f');
     mover('f', TEMPO);
     stopMotors();
 
+    delay(2000);
+
+    boostMotor('e', 't');
+    boostMotor('d', 'f');
     mover('e', TEMPO/5);
     stopMotors();
 
+    delay(2000);
+
+    boostMotor('e', 'f');
+    boostMotor('d', 'f');
     mover('f', 0);
     while(valSensor()==1);
     stopMotors();
@@ -144,34 +234,58 @@ int valSensor(){
   return int(digitalRead(SENOP));
 }
 
-int calDist() {
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+void seguidorDeLinha(){
+  static int estado=0, velOutLine1=0, velOutLine2=55, velInLine1=0, velInLine2=35;
+  static boolean darBoost=true;
+  static unsigned int tempo=millis();
   
-  int distancia = (ultrasonic.Ranging(CM));
-  return distancia;
+  switch(estado){
+    case 0: if(darBoost){
+              if(velOutLine2>velOutLine1) boostMotor('d', 'f');
+              else boostMotor('e', 'f');
+      
+              darBoost=false;
+            }
+
+            if(millis()-tempo>=500){
+              if(velOutLine2>velOutLine1) velOutLine2=35;
+              else velOutLine1=35;
+            }
+            
+            moverMotor('e', velOutLine1);
+            moverMotor('d', velOutLine2);
+            
+            if(valSensor()==0){
+              estado=1;
+
+              if(velOutLine2>velOutLine1) velOutLine2=45;
+              else velOutLine1=45;
+            }
+            
+            break;
+            
+    case 1: moverMotor('e', velInLine1);
+            moverMotor('d', velInLine2);
+
+            if(valSensor()==1){
+              estado=0;
+
+              swap(&velOutLine1, &velOutLine2);
+
+              swap(&velInLine1, &velInLine2);
+
+              darBoost=true;
+
+              tempo=millis();
+            }
+            
+            break;
+  }
 }
 
-void moverMotor(double left, double right){
-  if(left>0){
-    digitalWrite(Motor_E1, LOW);
-    digitalWrite(Motor_E2, HIGH);
-  }else{
-    digitalWrite(Motor_E1, HIGH);
-    digitalWrite(Motor_E2, LOW);
-  }
+void loop() {
+  int distancia=calDist(false);
 
-  if(right>0){
-    digitalWrite(Motor_D1, HIGH);
-    digitalWrite(Motor_D2, LOW);
-  }else{
-    digitalWrite(Motor_D1, LOW);
-    digitalWrite(Motor_D2, HIGH);
-  }
-
-  analogWrite(Motor_EV, abs(left));
-  analogWrite(Motor_DV, abs(right));
+  if(distancia<=8) desviarObstaculo();
+  else seguidorDeLinha();
 }
